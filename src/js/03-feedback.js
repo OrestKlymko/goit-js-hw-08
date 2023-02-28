06f1fd58d5af772447e878f5f8554b9d6e42a33f
OrestKlymko <orest.klymko2020@gmail.com>
2023-02-28 09:35:01 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,37 +4,37 @@ const form = document.querySelector('.feedback-form')
 const textarea = document.querySelector('.feedback-form textarea')
 const input = document.querySelector('.feedback-form input')
 const saveItem = localStorage.getItem('formdata');
+let temp = JSON.parse(saveItem);
 reload();
 const formData ={};
 
 
 form.addEventListener('submit', ((e)=>{
 	e.preventDefault();
-  console.log(`Email: ${formData.email}, Message: ${formData.message}`)
+  console.log(`Email: ${JSON.parse(localStorage.getItem('formdata')).email}, Message: ${JSON.parse(localStorage.getItem('formdata')).message}`)
 	e.target.reset();
 	localStorage.removeItem('formdata')
 }))
 
 function onChangeInput(e){
-	formData[e.target.name]=e.target.value;
-	localStorage.setItem('formdata', JSON.stringify(formData));
+if(saveItem){
+  for (let key in temp){
+    formData[key] = temp[key];
+  }
 }
+  formData[e.target.name]=e.target.value;
+	localStorage.setItem('formdata', JSON.stringify(formData));
+  }
+
+
 form.addEventListener('input',throttle(onChangeInput,500))
 
 
 function reload () {
-
-
-  if (saveItem) {
-    let temp = JSON.parse(saveItem);
-    if(saveItem){
-      if (temp.email){
-        input.value = temp.email;
-      }
-    if (temp.message) {
-      textarea.value = temp.message;
-    }
-    }
+if(saveItem){
+  for (let key in temp){
+    form.elements[key].value = temp[key];
   }
   }
+}
 
